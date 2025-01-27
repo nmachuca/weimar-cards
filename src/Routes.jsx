204f3979c1f622e58cdce8134104f9b06bd4088c
nmachuca <nmachuca@protonmail.com>
2025-01-27 12:36:27 -0300

    fix: switch to routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Routes, Redirect } from "react-router-dom";
 import App from './App.jsx'
 import { Gallery } from './Gallery.jsx';
 
 function Routes() {
   return (
-    <Switch>
-      <Route exact path="/" element={<App />} />
-      <Route exact path="/gallery" element={<Gallery />} />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/gallery" element={<Gallery />} />
       <Route path="*" render={() => <Redirect to={"/"} />} />
-    </Switch>
+    </Routes>
   );
 };
 
