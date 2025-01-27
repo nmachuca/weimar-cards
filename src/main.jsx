a827e59e8d3d011416c8463a34ec3df1e6df7590
nmachuca <nmachuca@protonmail.com>
2025-01-27 12:38:12 -0300

    fix import

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import { Gallery } from './Gallery.jsx';
 import "./styles/reset.css";
 import "./assets/fontawesome-pro-6.7.1-web/css/all.css"
-import Routes from './Routes';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
